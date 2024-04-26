54b4231a7f68e09adb0f4126a62a868f7b7fd2d6
robin jacobsson <robin.jacobsson@kebne.se>
2024-04-26 20:32:38 +0200

    Adjust tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,42 +2,11 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
     purge: [],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
-        },
         fontFamily: {
             sans: ['Graphik', 'sans-serif'],
             serif: ['Merriweather', 'serif'],
         },
-        extend: {
-            colors: {
-                gray: colors.coolGray,
-                blue: colors.sky,
-                red: colors.rose,
-                pink: colors.fuchsia,
-            },
-            spacing: {
-                '128': '32rem',
-                '144': '36rem',
-            },
-            borderRadius: {
-                '4xl': '2rem',
-            },
-            minHeight: {
-                '664': '664px'
-            }
-        }
-    },
-    variants: {
-        extend: {
-            borderColor: ['focus-visible'],
-            opacity: ['disabled'],
-        }
     },
     plugins: [],
 }
