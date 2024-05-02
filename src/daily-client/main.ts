92bf8b8d7403aaea2b8f00a42b90c884787df863
robin jacobsson <robin.jacobsson@kebne.se>
2024-05-02 12:06:48 +0200

    Remove ts stuff

--- src/daily-client/main.ts
+++ src/daily-client/main.ts
@@ -1,23 +1,6 @@
 // @ts-ignore
 import { Elm } from "./elm/Main.elm";
 
-import view_state from "./test/view_state_2.json";
-
-const projects = Object.entries(view_state).map(([projectName, branch]) => ({
-  name: projectName,
-  branches: Object.entries(branch).map(([branchSlug, data]) => ({
-    name: data.name,
-    slug: branchSlug,
-    date: data.date,
-  })),
-}));
-
 Elm.Main.init({
-  flags: {
-    owner: "robinjac",
-    hostRepository: "daily-sites",
-    selectedProject: projects[0],
-    projects,
-  },
   node: document.getElementById("app") as HTMLElement,
 });
